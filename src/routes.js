3de0d5d8266cec6b2c937103ec02447c17d0ad4e
Robson Kades <robsonkades@outlook.com>
2020-03-10 20:40:00 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Ok' }));
+routes.post('/users', UserController.store);
 
 module.exports = routes;
