f27919800df7c8daee20b50b6e01e8ed04b2f5a1
Robson Kades <robsonkades@outlook.com>
2020-03-10 20:17:29 -0300

    Model de usuário

--- src/app.js
+++ src/app.js
@@ -2,19 +2,19 @@ import express from 'express';
 import routes from './routes';
 
 class App {
-    constructor() {
-        this.server = express();
-        this.middlewares();
-        this.routes();
-    }
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
 
-    middlewares() {
-        this.server.use(express.json());
-    }
+  middlewares() {
+    this.server.use(express.json());
+  }
 
-    routes() {
-        this.server.use(routes);
-    }
+  routes() {
+    this.server.use(routes);
+  }
 }
 
 export default new App().server;
