95be68102dd6f2c3dc25d70d4e5b8d8ffe79bf0a
Robson Kades <robsonkades@outlook.com>
2020-03-10 22:34:49 -0300

    Validando dados de entrada

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,23 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validação falhou!' });
+    }
+
     const { email, password } = req.body;
 
     const user = User.findOne({ where: { email } });
