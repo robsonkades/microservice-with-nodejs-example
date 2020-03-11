d589d9571616ab75a3d216fa7c473b22d595dbd8
Robson Kades <robsonkades@outlook.com>
2020-03-10 22:09:38 -0300

    Middleware de autenticação

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token não autorizado!' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { id, name, email } = await promisify(jwt.verify)(
+      token,
+      authConfig.secret
+    );
+    req.user = { id, name, email };
+  } catch (error) {
+    return res.status(401).json({ error: 'Token não autorizado!' });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 module.exports = routes;
