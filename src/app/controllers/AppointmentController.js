d28689ed259a5d1783a23640364521d7decf0814
Robson Kades <robsonkades@outlook.com>
2020-03-12 18:04:34 -0300

    Validações de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import User from '../models/User';
 import Appointment from '../models/Appointment';
@@ -24,10 +25,30 @@ class AppointmentController {
       return res.status(401).json({ error: 'Acesso negado' });
     }
 
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'Não é permitido informar uma data no passado.' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Horário não disponível' });
+    }
+
     const appointment = Appointment.create({
       user_id: req.user.id,
       provider_id,
-      date,
+      hourStart,
     });
 
     return res.json(appointment);
